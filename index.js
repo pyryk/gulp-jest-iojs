8529942e72c2a3f66e41d1b076e6dea2847033a1
Pyry Kröger <pyry.kroger@futurice.com>
2016-01-22 17:23:50 +0200

    add experimental support for passing CLI args

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var jest    = require('jest-cli'),
     gutil   = require('gulp-util'),
     through = require('through2');
 
-module.exports = function (options) {
+module.exports = function (options, jestOptions) {
     options = options || {};
 
     return through.obj(function (file, enc, cb) {
         options.rootDir = options.rootDir || file.path;
 
-        jest.runCLI({
-            config: options
-        }, options.rootDir, function (success) {
+        const opts = Object.assign({}, jestOptions, {config: options});
+
+        jest.runCLI(opts, options.rootDir, function (success) {
             if(!success) {
                 cb(new gutil.PluginError('gulp-jest-iojs', { message: "Tests Failed" }));
             } else {
